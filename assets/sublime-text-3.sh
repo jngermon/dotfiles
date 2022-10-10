058d69320dae1fd5a65e03e801c4410391bd7176
jngermon <jn.germon@gmail.com>
2022-10-10 16:13:33 +0200

    Update sublime-text-3 dependencies

--- assets/sublime-text-3.sh
+++ assets/sublime-text-3.sh
@@ -8,21 +8,12 @@ else
 
     SUBLIME="$(pwd)/files/sublime-text-3"
     SUBLIME_CONFIG="$SUBLIME/config"
-    SUBLIME_CONFIG_DEST="$HOME/.config/sublime-text-3/Packages/User"
+    SUBLIME_CONFIG_DEST="$HOME/.config/sublime-text/Packages/User"
 
     createSymlink $SUBLIME_CONFIG $SUBLIME_CONFIG_DEST
 
     if [ $(which composer) ]; then
         composer --working-dir=$(pwd)/files/sublime-text-3 -- install
-
-        HOME_BIN=$HOME/bin
-
-        mkdirIfNotExists $HOME_BIN
-
-        createSymlink  $SUBLIME/vendor/pdepend/pdepend/src/bin/pdepend $HOME_BIN/pdepend
-        createSymlink  $SUBLIME/vendor/squizlabs/php_codesniffer/scripts/phpcs $HOME_BIN/phpcs
-        createSymlink  $SUBLIME/vendor/friendsofphp/php-cs-fixer/php-cs-fixer $HOME_BIN/php-cs-fixer
-        createSymlink  $SUBLIME/vendor/phpmd/phpmd/src/bin/phpmd $HOME_BIN/phpmd
     fi
 
     exit 0
