f1baae60a431fcac9299f6653cfedaf3ddc3f6a3
jngermon <jn.germon@gmail.com>
2025-01-30 09:15:46 +0100

    FIx nvim download url

--- assets/nvim-install.sh
+++ assets/nvim-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
-sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
+sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin
 
 . "$(dirname $0)/common.sh"
 
