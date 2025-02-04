f288733fa4fd10a38df382d281fcbb17484f2939
jngermon <jn.germon@gmail.com>
2025-02-04 13:36:04 +0100

    Add install instructions for lazygit

--- assets/nvim-install.sh
+++ assets/nvim-install.sh
@@ -13,3 +13,15 @@ NVIMCONFIG="$(pwd)/files/nvim/nvim.desktop"
 NVIMCONFIG_DEST="$HOME/.local/share/applications/nvim.desktop"
 
 createSymlink $NVIMCONFIG $NVIMCONFIG_DEST
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit
+rm lazygit.tar.gz
+
+LAZYGITCONFIG="$(pwd)/files/lazygit/config.yml"
+LAZYGITCONFIG_DEST="$HOME/.config/lazygit/config.yml"
+
+createSymlink $LAZYGITCONFIG $LAZYGITCONFIG_DEST
