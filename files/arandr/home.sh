d2256dbbfe5a6011e379398a9ec9699a58152147
jngermon <jn.germon@gmail.com>
2025-09-25 12:55:08 +0200

    Arandr save several configurations

--- files/arandr/home.sh
+++ files/arandr/home.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-xrandr --output DP-0 --mode 1920x1080 --pos 0x420 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --primary --mode 1920x1080 --pos 1920x420 --rotate normal --output DP-3 --off --output HDMI-1 --off --output DP-4 --mode 1920x1080 --pos 3840x0 --rotate right --output DP-5 --off
+center2.sh
